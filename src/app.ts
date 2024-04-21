54213a69700405e7d5f07c9cc46225b281005382
Nathan George <georgenathan010@gmail.com>
2024-04-21 14:57:59 +0300

    added connection functionality

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log("hello world")
+import {connectDb} from './utils/connectDb';
+import {PORT, MONGO_URI} from './config/connection';
+import http from 'http';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import express from 'express';
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(compression());
+app.use(cookieParser());
+
+
+//connect 
+const server = http.createServer(app);
+
+
+
+const makeConnection =  async () => {
+    try {
+        await connectDb(MONGO_URI);
+        server.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+
+    } catch(e) {
+        console.error(e);
+    }
+}
--- src/config/connection.ts
+++ src/config/connection.ts
@@ -3,3 +3,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+
+export {
+    PORT,
+    MONGO_URI
+};
